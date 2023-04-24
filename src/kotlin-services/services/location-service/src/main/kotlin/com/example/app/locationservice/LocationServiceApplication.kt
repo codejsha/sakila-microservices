5beecbe60a56a4d97fbd458c7f42879136da9092
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-04-24 20:40:22 +0900

    add location service

---
+++ src/kotlin-services/services/location-service/src/main/kotlin/com/example/app/locationservice/LocationServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.app.locationservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LocationServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<LocationServiceApplication>(*args)
+}
