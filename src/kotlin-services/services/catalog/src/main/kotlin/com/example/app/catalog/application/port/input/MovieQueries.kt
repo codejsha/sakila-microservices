85881e2e08a8b695b843bdfa19be3306a459bcf2
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-10-10 01:49:04 +0900

    feat: update movie search service

--- src/kotlin-services/services/catalog/src/main/kotlin/com/example/app/catalog/application/port/input/MovieQueries.kt
+++ src/kotlin-services/services/catalog/src/main/kotlin/com/example/app/catalog/application/port/input/MovieQueries.kt
@@ -1,6 +1,7 @@
 package com.example.app.catalog.application.port.input
 
 import com.example.shared.application.port.BaseQuery
+import com.example.shared.infrastructure.adapter.web.ElementRequest
 
 data class MovieGetQuery(val id: Int) : BaseQuery()
-class MovieListGetQuery : BaseQuery()
+class MovieListGetQuery(val elementRequest: ElementRequest) : BaseQuery()
